0176afd5f8e9066e71b0f00cfc4f29045017e028
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-06-27 14:24:47 +0530

    Time: 18 ms (12.76%), Space: 52.5 MB (85.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int n=nums.length;
+        PriorityQueue<Integer> p=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            p.add(nums[i]);
+        }
+        int i=0;
+        int m=n/2;
+        while(i<m){
+            p.remove();
+            i++;
+        }
+        return p.remove();
     }
 }
