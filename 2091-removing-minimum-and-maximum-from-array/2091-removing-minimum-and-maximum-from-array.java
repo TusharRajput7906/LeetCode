c39b987417f7372da633199d6306e71f71e7a639
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-08-30 14:52:29 +0530

    Time: 4 ms (10.73%), Space: 86.8 MB (40%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -5,48 +5,6 @@ class Solution {
             return n;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        if(n%2!=0){
-            for (int i = 0; i < n; i++) {
-            if (min > nums[i]) {
-                min = nums[i];
-            }
-            if (max < nums[i]) {
-                max = nums[i];
-            }
-        }
-        int left = 0;
-        int right = 0;
-        int c = 0;
-        for (int i = 0; i < n / 2; i++) {
-            if (max == nums[i]) {
-                left = i + 1;
-                c++;
-            } else if (min == nums[i]) {
-                left = i + 1;
-                c++;
-            }
-        }
-        if (c == 2) {
-            return left;
-        }
-        c = 0;
-        for (int i = n - 1; i >= n / 2; i--) {
-            if (max == nums[i]) {
-                right = n - i;
-                c++;
-            } else if (min == nums[i]) {
-                right = n - i;
-                c++;
-            }
-        }
-        if (c == 2) {
-            return right;
-        }
-        int both=left+right;
-        int leftSide=(n/2)+(n/2-right+2);
-        int rightSide=n/2+1+(n/2-left+1);
-        return both>leftSide?(leftSide>rightSide?rightSide:leftSide):(both>rightSide?rightSide:both);
-        }
         for (int i = 0; i < n; i++) {
             if (min > nums[i]) {
                 min = nums[i];
@@ -83,9 +41,16 @@ class Solution {
         if (c == 2) {
             return right;
         }
-        int both=left+right;
-        int leftSide=(n/2)+(n/2-right+1);
-        int rightSide=n/2+(n/2-left+1);
-        return both>leftSide?(leftSide>rightSide?rightSide:leftSide):(both>rightSide?rightSide:both);
+        int both = left + right;
+        int leftSide = 0;
+        int rightSide = 0;
+        if (n % 2 != 0) {
+            leftSide = (n / 2) + (n / 2 - right + 2);
+            rightSide = n / 2 + 1 + (n / 2 - left + 1);
+        } else {
+            leftSide = (n / 2) + (n / 2 - right + 1);
+            rightSide = n / 2 + (n / 2 - left + 1);
+        }
+        return both > leftSide ? (leftSide > rightSide ? rightSide : leftSide) : (both > rightSide ? rightSide : both);
     }
 }
