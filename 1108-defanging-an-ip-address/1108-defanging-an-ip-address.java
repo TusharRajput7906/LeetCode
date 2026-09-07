a0207a8eaab4a226d8c1921efd8cad69c99ccacd
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-07 10:35:02 +0530

    Time: 0 ms (100%), Space: 43 MB (25.23%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<address.length();i++){
+            char ch=address.charAt(i);
+            if(ch!='.'){
+                str.append(ch);
+            }else{
+                str.append('[');
+                str.append(ch);
+                str.append(']');
+            }
+        }
+        return str.toString();
+    }
+}
