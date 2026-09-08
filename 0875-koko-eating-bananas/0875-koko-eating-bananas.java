43dc5a4eb7fb6f05d545d452d2912a2e954f92ed
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-08 15:24:40 +0530

    Time: 12 ms (49.42%), Space: 48 MB (43.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,24 +1,36 @@
 class Solution {
+    public boolean rev(int[] arr,int mid,int h){
+        long ans=0;
+            for (int i = 0; i < arr.length; i++) {
+           if(arr[i]%mid==0){
+                ans+=(arr[i]/mid);
+            }else{
+                ans+=(arr[i]/mid)+1;
+            }
+        }
+        if(ans<=h) return true;
+        return false;
+    }
+    public int find(int[] arr,int h,int low,int high){
+        int res=Integer.MAX_VALUE;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(rev(arr,mid,h)){
+                    res=Math.min(res,mid);
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                
+            }
+            }
+            return res;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = 0;
-        for (int pile : piles) {
-            high = Math.max(pile, high);
+        int low=1;
+        int high=-1;
+        for(int n:piles){
+            high=Math.max(high,n);
         }
-        while (low <= high) {
-            long hours = 0;
-            int mid = low + (high - low) / 2;
-            for (int pile : piles) {
-                hours += (pile / mid);
-                if (pile % mid != 0)
-                    hours += 1;
-            }
-            if (hours <= h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
+        return find(piles,h,low,high);
     }
 }
