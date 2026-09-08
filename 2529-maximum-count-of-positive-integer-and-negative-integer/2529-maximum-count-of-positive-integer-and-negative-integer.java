91e83739c6469fefa2c02cd372cad6ad15430a28
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-08 13:53:49 +0530

    Time: 0 ms (100%), Space: 47 MB (38.46%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,11 +1,75 @@
 class Solution {
-    public int maximumCount(int[] nums) {
-        int neg=0,pos=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0) pos++;
-            else if(nums[i]<0) neg++;
+
+    public int neg(int left, int right, int[] arr) {
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Found first negative element
+            if (arr[mid] < 0) {
+
+                if (mid == arr.length - 1 || arr[mid + 1] >= 0) {
+                    return mid;
+                }
+
+                left = mid + 1;
+
+            } else {
+                right = mid - 1;
+            }
         }
-        if(pos>neg) return pos;
-        return neg;
+
+        return -1;
+    }
+
+    public int pos(int left, int right, int[] arr) {
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Found first positive element
+            if (arr[mid] > 0) {
+
+                if (mid == 0 || arr[mid - 1] <= 0) {
+                    return mid;
+                }
+
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public int maximumCount(int[] nums) {
+
+        int left = 0, right = nums.length - 1;
+
+        int pos = pos(left, right, nums);
+
+        int positive;
+
+        if (pos == -1) {
+            positive = 0;
+        } else {
+            positive = nums.length - pos;
+        }
+
+        int neg = neg(left, right, nums);
+
+        int negative;
+
+        if (neg == -1) {
+            negative = 0;
+        } else {
+            negative = neg + 1;
+        }
+
+        return positive > negative ? positive : negative;
     }
 }
