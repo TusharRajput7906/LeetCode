aa84f99158aaf03dff60a420d19f69cd1e415929
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-09 11:49:21 +0530

    Time: 2 ms (37.87%), Space: 42.5 MB (6.9%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
@@ -1,22 +1,33 @@
 class Solution {
+    public int len(int n){
+        int count=0;
+        while(n>0){
+            n/=10;
+            count++;
+        }
+        return count;
+    }
     public int splitNum(int num) {
-        String str=Integer.toString(num);
-        int[] arr=new int[str.length()];
-        for(int i=0;i<str.length();i++){
-            arr[i]=str.charAt(i)-'0';
+        // String str = Integer.toString(num);
+        int[] arr = new int[len(num)];
+        int l=0;
+        while(num>0){
+            arr[l]=num%10;
+            num/=10;
+            l++;
         }
         Arrays.sort(arr);
-        int val=0,val1=0;
-        int n=str.length();
-        int i=0;
-        while(i<n){
-            if(i%2==0){
-            val=val*10+arr[i];
-            }else{
-                val1=val1*10+arr[i];
+        int val = 0, val1 = 0;
+        int n = arr.length;
+        int i = 0;
+        while (i < n) {
+            if (i % 2 == 0) {
+                val = val * 10 + arr[i];
+            } else {
+                val1 = val1 * 10 + arr[i];
             }
             i++;
         }
-     return val+val1;   
+        return val + val1;
     }
 }
