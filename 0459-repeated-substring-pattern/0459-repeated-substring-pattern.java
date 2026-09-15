2041b7c046c167b56b4b482a35702cc75ac75cc4
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-15 14:53:04 +0530

    Time: 76 ms (25.69%), Space: 46.9 MB (49.2%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-       String dbl=s+s;
-       return dbl.substring(1,dbl.length()-1).contains(s);
+        String dbl = s + s;
+        return dbl.substring(1, dbl.length() - 1).contains(s);
     }
 }
