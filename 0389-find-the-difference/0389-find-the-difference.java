65bae850bf96cf603acc11717e47592f70f66325
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-15 13:57:40 +0530

    Time: 12 ms (11.73%), Space: 42.9 MB (84.67%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,19 +1,34 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] arr=new int[26];
+        HashMap<Character,Integer> h=new HashMap<>();
         for(int i=0;i<t.length();i++){
-            int idx=t.charAt(i)-'a';
-            arr[idx]++;
+            char ch=t.charAt(i);
+            h.put(ch,h.getOrDefault(ch,0)+1);
         }
         for(int i=0;i<s.length();i++){
-            int idx=s.charAt(i)-'a';
-            arr[idx]--;
+            char ch=s.charAt(i);
+            h.put(ch,h.getOrDefault(ch,0)-1);
         }
-        for(int i=0;i<26;i++){
-            if(arr[i]==1){
-                return (char)(i+97);
+        for(char key:h.keySet()){
+            if(h.get(key)==1){
+                return key;
             }
         }
         return 'a';
+        // int[] arr=new int[26];
+        // for(int i=0;i<t.length();i++){
+        //     int idx=t.charAt(i)-'a';
+        //     arr[idx]++;
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     int idx=s.charAt(i)-'a';
+        //     arr[idx]--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(arr[i]==1){
+        //         return (char)(i+97);
+        //     }
+        // }
+        // return 'a';
     }
 }
