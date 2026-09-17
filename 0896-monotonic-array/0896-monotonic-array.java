5468fdcd6cab5640d851b02870e01cc75b1bc204
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-17 11:09:57 +0530

    Time: 4 ms (4.23%), Space: 85.5 MB (7.45%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int i=0,j=1;
+        int n=nums.length;
+        boolean inc=true;
+        boolean dec=true;
+        while(j<n){
+            if(!(nums[i]<=nums[j])){
+                inc=false;
+            }
+            j++;i++;
+        }
+        i=0;
+        j=1;
+        while(j<n){
+            if(!(nums[i]>=nums[j])){
+                dec=false;
+            }
+            j++;i++;
+        }
+        if(inc || dec){
+            return true;
+        }
+        return false;
+    }
+}
