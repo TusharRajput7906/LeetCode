255b891c39e3cbb0ab871737984671ea60c503ab
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-17 14:06:10 +0530

    Time: 10 ms (59.37%), Space: 47 MB (43.86%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,23 +1,23 @@
 class Solution {
     public int maximumUnits(int[][] arr, int truckSize) {
-        Arrays.sort(arr,(a,b)->{
-            return b[1]-a[1];
+        Arrays.sort(arr, (a, b) -> {
+            return b[1] - a[1];
         });
-        int n=arr.length;
-        int m=arr[0].length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            int pro=1;
-            if(truckSize==0) return sum;
-            if(truckSize>=arr[i][0]){
-                pro=arr[i][0]*arr[i][1];
-                truckSize-=arr[i][0];
-            }else if(arr[i][0]>truckSize){
-                    // int val=arr[i][0]-truckSize;
-                    pro=truckSize*arr[i][1];
-                    truckSize=0;
+        int n = arr.length;
+        int m = arr[0].length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int pro = 1;
+            if (truckSize == 0)
+                return sum;
+            if (truckSize >= arr[i][0]) {
+                pro = arr[i][0] * arr[i][1];
+                truckSize -= arr[i][0];
+            } else if (arr[i][0] > truckSize) {
+                pro = truckSize * arr[i][1];
+                truckSize = 0;
             }
-            sum+=pro;
+            sum += pro;
         }
         return sum;
     }
