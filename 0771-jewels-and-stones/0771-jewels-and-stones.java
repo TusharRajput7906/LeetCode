f933cd17af56d2ba6898e187556513f736231f17
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-18 10:58:28 +0530

    Time: 0 ms (100%), Space: 43.5 MB (8.6%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         List<Character> li=new ArrayList<>();
+//         for(int i=0;i<jewels.length();i++){
+//             char ch=jewels.charAt(i);
+//             li.add(ch);
+//         }
+//         int ans=0;
+//         for(int i=0;i<stones.length();i++){
+//             char ch=stones.charAt(i);
+//             if(li.contains(ch)){
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        List<Character> li=new ArrayList<>();
-        for(int i=0;i<jewels.length();i++){
-            char ch=jewels.charAt(i);
-            li.add(ch);
+        int[] arr = new int[128];
+        int count = 0;
+        for(int i : jewels.toCharArray()){
+            arr[i] = 1;
         }
-        int ans=0;
-        for(int i=0;i<stones.length();i++){
-            char ch=stones.charAt(i);
-            if(li.contains(ch)){
-                ans++;
+        for(int j : stones.toCharArray()){
+            if(arr[j]==1){
+                count++;
             }
         }
-        return ans;
+        return count;
     }
 }
