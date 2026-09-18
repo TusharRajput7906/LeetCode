71c07152a9fb462cbc9ec86e7cb97e9fda448a69
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-18 12:05:58 +0530

    Time: 1 ms (100%), Space: 46.6 MB (11.52%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
@@ -1,25 +1,30 @@
 class Solution {
-    public boolean vowel(String str){
-        int i=0,j=str.length()-1;
-        if((str.charAt(i)=='a' &&( str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
-        ||
-        (str.charAt(i)=='e' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
-        ||
-        (str.charAt(i)=='i' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
-        ||
-        (str.charAt(i)=='o' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
-        ||
-        (str.charAt(i)=='u' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
-        ){
-            return true;
+    public boolean vowel(char ch) {
+        // int i=0,j=str.length()-1;
+        // if((str.charAt(i)=='a' &&( str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
+        // ||
+        // (str.charAt(i)=='e' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
+        // ||
+        // (str.charAt(i)=='i' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
+        // ||
+        // (str.charAt(i)=='o' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
+        // ||
+        // (str.charAt(i)=='u' && (str.charAt(j)=='a'||str.charAt(j)=='e'||str.charAt(j)=='i'||str.charAt(j)=='o'||str.charAt(j)=='u'))
+        // ){
+        //     return true;
 
-        }
-        return false;
+        // }
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
+
     public int vowelStrings(String[] words, int left, int right) {
-        int ans=0;
-        for(int i=left;i<=right;i++){
-            if(vowel(words[i])){
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            String str = words[i];
+            int len = str.length();
+            char first = str.charAt(0);
+            char sec = str.charAt(len - 1);
+            if (vowel(first) && vowel(sec)) {
                 ans++;
             }
         }
