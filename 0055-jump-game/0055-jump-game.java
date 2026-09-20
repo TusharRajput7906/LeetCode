b650dc476fad1e8e12448a0aa2ab6d23b70da7e0
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-09-20 22:24:52 +0530

    Time: 2 ms (89.52%), Space: 48.1 MB (23.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return true;
+        int maxIdx = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > maxIdx) {
+                return false;
+            } else {
+                maxIdx = Math.max(maxIdx, i + nums[i]);
+            }
+        }
+        return true;
+    }
+}
